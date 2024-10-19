fba4e44635b298fe6126e7d9572465a0173ddca2
DineshKuddana <dineshkuddana@gmail.com>
2024-10-19 09:28:46 +0530

    remove admin routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import React, { useState, useEffect } from "react";
+import React from "react";
 import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -14,37 +14,11 @@ import Blog from './sections/Blog/BlogGrid';
 import BlogDetail from "./sections/Blog/BlogDetail";
 
 import Courses from "./sections/Courses/Courses";
-import AdminLogin from "../src/components/Admin/AdminLogin";
-import AdminConsole from "../src/components/Admin/AdminConsole";
+
 
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    // Check local storage to see if the user is already authenticated
-    const savedAuthState = localStorage.getItem('isAuthenticated');
-    if (savedAuthState === 'true') {
-      setIsAuthenticated(true);
-    }
-  }, []);
-
-  const handleLogin = (username, password) => {
-    const correctUsername = 'admin';
-    const correctPassword = 'password123'; 
-
-    if (username === correctUsername && password === correctPassword) {
-      setIsAuthenticated(true);
-      localStorage.setItem('isAuthenticated', 'true'); 
-    } else {
-      alert('Invalid username or password');
-    }
-  };
-
-  const handleLogout = () => {
-    setIsAuthenticated(false);
-    localStorage.removeItem('isAuthenticated'); 
-  };
+ 
 
   return (
     <div className="App">
@@ -60,16 +34,7 @@ function App() {
           
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog/:id" element={<BlogDetail />} />
-          <Route
-            path="/admin/*"
-            element={
-              isAuthenticated ? (
-                <AdminConsole onLogout={handleLogout} />
-              ) : (
-                <AdminLogin onLogin={handleLogin} />
-              )
-            }
-          />
+          
         </Routes>
       </Router>
     </div>
